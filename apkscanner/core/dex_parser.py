a5582156133259ee0d05959bf3f701d2fc1a5678
cx <jasmine_bear@outlook.com>
2021-04-19 18:27:57 +0800

    add dex parser

---
+++ apkscanner/core/dex_parser.py
@@ -0,0 +1,30 @@
+# coding:utf8
+'''
+@File    :   dex_parser.py
+@Author  :   Loopher 
+@Version :   1.0
+@License :   (C)Copyright 2020-2021,Loopher
+@Desc    :   Dex文件解析
+'''
+"""
+每一个dex都会经过这里的解析处理，目的是建立一个映射表能快速索引和比较
+"""
+
+
+
+
+import io
+from apkscanner.parser.base_parser import BaserParser
+from apkscanner.utils.buffer import BuffHandle
+class DexParser(BaserParser):
+
+    parser_info = {
+        "name": "DexParser",
+        "desc": "Parsing Dex file into bytecode"
+    }
+
+    def __init__(self, dex, buff=None):
+
+        super(DexParser, self).__init__(dex, buff)
+
+        self.fp = BuffHandle(self.buff)
